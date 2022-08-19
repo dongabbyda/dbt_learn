fd473be0e47a16f3f76f7b2acc19f5af746264bf
dongabbyda <76711699+dongabbyda@users.noreply.github.com>
2022-08-19 01:30:08 +0100

    my first comit

---
+++ dbt_fundamental/models/staging/stripe/stg_payments.sql
@@ -0,0 +1,12 @@
+with payments as (
+    select 
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status,
+        amount/100 as amount,   --amount is stored in cents, convert it to dollars
+        created as created_at
+    from {{source('stripe','payments')}} ---trainingdb.gabyschema.payments
+)
+
+select * from payments
